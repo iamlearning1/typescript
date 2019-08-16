ac14b61529739de2ba6c0ad48bbd950933fd3bbb
Deepak Kumar <d1612k@gmail.com>
2019-08-16 23:57:57 +0530

    basic types

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 "use strict";
-console.log("working");
+var myString = "deepak";
+// myString = 12;
+var myNumber = 12;
+// myNumber = false
+var hobby = false;
+// hobby = 12;
+// type: any
+var age;
+age = 27;
+age = "None";
+var myAge;
+// myAge = 'red'
+// array
+var hobbies = ["reading", "learning"];
+// hobbies = [100];
+// tuples
+var address = ["street", 2];
+// enums
+var Color;
+(function (Color) {
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 101] = "Blue";
+    Color[Color["Red"] = 102] = "Red";
+})(Color || (Color = {}));
+var myColor = Color.Green;
+console.log(myColor);
--- app.ts
+++ app.ts
@@ -1,1 +1,33 @@
-console.log("working");
+let myString: string = "deepak";
+// myString = 12;
+
+let myNumber: number = 12;
+// myNumber = false
+
+let hobby: boolean = false;
+// hobby = 12;
+
+// type: any
+let age: any;
+age = 27;
+age = "None";
+
+let myAge: number;
+// myAge = 'red'
+
+// array
+let hobbies: string[] = ["reading", "learning"];
+// hobbies = [100];
+
+// tuples
+let address: [string, number] = ["street", 2];
+
+// enums
+enum Color {
+  Green = 100,
+  Blue,
+  Red
+}
+
+const myColor: Color = Color.Green;
+console.log(myColor);
