1531c029d7fe4bba4fc02280d4c5667f76e8fbfa
Deepak Kumar <d1612k@gmail.com>
2019-08-18 21:07:15 +0530

    inheritance

--- dist/app.js
+++ dist/app.js
@@ -2,8 +2,22 @@
 function greet(person) {
     console.log("Hi", person.name);
 }
-greet({
+// greet({
+//   name: "Deepak",
+//   age: 24,
+//   hobbies: ["reading"]
+// });
+var Human = /** @class */ (function () {
+    function Human(name) {
+        this.name = name;
+    }
+    Human.prototype.greet = function () {
+        console.log("Hi", this.name);
+    };
+    return Human;
+}());
+var me = new Human("Deepak");
+var senior = {
     name: "Deepak",
-    age: 24,
-    hobbies: ["reading"]
-});
+    age: 24
+};
--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,31 @@ function greet(person: Person): void {
   console.log("Hi", person.name);
 }
 
-greet({
+// greet({
+//   name: "Deepak",
+//   age: 24,
+//   hobbies: ["reading"]
+// });
+
+class Human implements Person {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  public greet(): void {
+    console.log("Hi", this.name);
+  }
+}
+
+const me = new Human("Deepak");
+// me.greet();
+
+interface Senior extends Person {
+  age: number;
+}
+
+const senior: Senior = {
   name: "Deepak",
-  age: 24,
-  hobbies: ["reading"]
-});
+  age: 24
+};
