3cdbcaa79e407873b9cf0a9bd774fcf5b89ad529
Deepak Kumar <d1612k@gmail.com>
2019-08-19 22:30:24 +0530

    first commit

--- dist/app.js
+++ dist/app.js
@@ -1,28 +1,19 @@
 "use strict";
-function echo(data) {
-    return data;
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function personDecorator(constructor) {
+    console.log("Person Class", constructor);
 }
-// console.log(betterEcho<string>("Deepak").length);
-// console.log(betterEcho<number>(24));
-// console.log(betterEcho<Person>({ name: "Deepak", age: 27 }));
-// Built-in generics
-var testResults = [2, 3];
-// Arrays
-// function print<Type>(arr: Type[]) {
-//   arr.forEach(item => console.log(item));
-// }
-// print<string>(["Apple", "Mango"]);
-// print<number>(testResults);
-// generic class
-var Simple = /** @class */ (function () {
-    function Simple(base, multiplier) {
-        this.base = base;
-        this.multiplier = multiplier;
+var Person = /** @class */ (function () {
+    function Person() {
+        console.log("Person");
     }
-    Simple.prototype.calculate = function () {
-        return +this.base * +this.multiplier;
-    };
-    return Simple;
+    Person = __decorate([
+        personDecorator
+    ], Person);
+    return Person;
 }());
-var simple = new Simple("12", 10);
-console.log(simple.calculate());
--- src/app.ts
+++ src/app.ts
@@ -1,49 +1,10 @@
-function echo(data: any) {
-  return data;
+function personDecorator(constructor: Function): void {
+  console.log("Person Class", constructor);
 }
 
-// console.log(echo("Deepak"));
-// console.log(echo(24));
-// console.log(echo({ name: "Deepak", age: 27 }));
-
-// function betterEcho<Type>(data: Type) {
-//   return data;
-// }
-
-interface Person {
-  name: string;
-  age: number;
-}
-
-// console.log(betterEcho<string>("Deepak").length);
-// console.log(betterEcho<number>(24));
-// console.log(betterEcho<Person>({ name: "Deepak", age: 27 }));
-
-// Built-in generics
-const testResults: Array<number> = [2, 3];
-
-// Arrays
-// function print<Type>(arr: Type[]) {
-//   arr.forEach(item => console.log(item));
-// }
-
-// print<string>(["Apple", "Mango"]);
-// print<number>(testResults);
-
-// generic class
-class Simple<T extends number | string, U extends number | string> {
-  base: T;
-  multiplier: U;
-
-  constructor(base: T, multiplier: U) {
-    this.base = base;
-    this.multiplier = multiplier;
-  }
-
-  calculate(): number {
-    return +this.base * +this.multiplier;
+@personDecorator
+class Person {
+  constructor() {
+    console.log("Person");
   }
 }
-
-const simple = new Simple<string, number>("12", 10);
-console.log(simple.calculate());
